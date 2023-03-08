99f162d434bad4e00054db2ec4e344343b1e22ae
Christian <chrisfehr16@gmail.com>
2023-03-08 17:31:53 +0100

    Fixed input validation

--- src/main/java/com/polynomjavafx/PolynomialController.java
+++ src/main/java/com/polynomjavafx/PolynomialController.java
@@ -6,9 +6,9 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
-
-import java.text.ParseException;
 
 public class PolynomialController {
     public Spinner<Double> coefficient5Spinner;
@@ -22,61 +22,87 @@ public class PolynomialController {
 
     private GraphicsContext graphicsContext;
 
-    private double xScale;
-    private double yScale;
+    private double xScale; //The amount of pixels on the canvas that represent one on the x-axis
+    private double yScale; //The amount of pixels on the canvas that represent one on the y-axis
 
     @FXML
-    private void initialize(){
+    private void initialize() {
         graphicsContext = polynomialCanvas.getGraphicsContext2D();
         this.yScale = polynomialCanvas.getHeight()/10;
         this.xScale = polynomialCanvas.getWidth()/10;
         initializeSpinners();
     }
-    private void initializeSpinners(){
-        coefficient0Spinner.setValueFactory(new SpinnerValueFactory.DoubleSpinnerValueFactory(-200, 200,0.0,0.01));
-        coefficient1Spinner.setValueFactory(new SpinnerValueFactory.DoubleSpinnerValueFactory(-200, 200,0.0,0.01));
-        coefficient2Spinner.setValueFactory(new SpinnerValueFactory.DoubleSpinnerValueFactory(-200, 200,0.0,0.01));
-        coefficient3Spinner.setValueFactory(new SpinnerValueFactory.DoubleSpinnerValueFactory(-200, 200,0.0,0.01));
-        coefficient4Spinner.setValueFactory(new SpinnerValueFactory.DoubleSpinnerValueFactory(-200, 200,0.0,0.01));
-        coefficient5Spinner.setValueFactory(new SpinnerValueFactory.DoubleSpinnerValueFactory(-200, 200,0.0,0.01));
+
+    private void initializeSpinners() {
+        Spinner<Double>[] spinners = new Spinner[] {coefficient0Spinner, coefficient1Spinner, coefficient2Spinner,
+                coefficient3Spinner, coefficient4Spinner, coefficient5Spinner};
+        for (Spinner<Double> spinner : spinners) {
+            //Set value factory
+            spinner.setValueFactory(new SpinnerValueFactory.DoubleSpinnerValueFactory(-200, 200, 0.0, 0.1));
+            //Add listener to textProperty so only valid input is accepted
+            spinner.getEditor().textProperty().addListener((observable, oldValue, newValue) -> {
+                        if (!newValue.matches("-?[0-9]+\\.?[0-9]*")) {
+                            if(newValue.equals("")){
+                                spinner.getEditor().setText("0");
+                            }
+                            else {
+                                spinner.getEditor().setText(oldValue);
+                            }
+                        }
+                    }
+            );
+        }
     }
+
     @FXML
     private void onSubmitButtonClicked() throws WrongInputSizeException {
-        try {
+        submitInput();
+    }
+    @FXML
+    private void onKeyPressed(KeyEvent keyEvent) throws WrongInputSizeException {
+        if(keyEvent.getCode() == KeyCode.ENTER){
+            submitInput();
+        }
+    }
+
+    private void submitInput() throws WrongInputSizeException {
         double[] coefficients = {coefficient0Spinner.getValue(), coefficient1Spinner.getValue(),
                 coefficient2Spinner.getValue(), coefficient3Spinner.getValue(),
                 coefficient4Spinner.getValue(), coefficient5Spinner.getValue()};
-            Polynom polynom = new Polynom(coefficients);
-            inputWarningLabel.setVisible(false);
-            drawPolynomialToCanvas(polynom, Color.RED);
-        }
-        catch (NumberFormatException invalidInput){
-            inputWarningLabel.setVisible(true);
-        }
-
+        Polynom polynom = new Polynom(coefficients);
+        drawPolynomialToCanvas(polynom, Color.RED);
     }
 
     private void drawPolynomialToCanvas(Polynom polynomialToDraw, Color color) {
         graphicsContext.setStroke(color);
+        //Set width of the drawn line
         double polynomialWidth = 1;
         graphicsContext.setLineWidth(polynomialWidth);
 
+        //Set the starting location of the line to the left side of the canvas
         double lastX = (-polynomialCanvas.getWidth() / xScale) / 2;
         double lastY = polynomialToDraw.functionValue(lastX);
 
-        for (double x = (-polynomialCanvas.getWidth() / 2) / xScale; x <= (polynomialCanvas.getWidth() / 2) / xScale; x += 0.1) {
+        for (double x = (-polynomialCanvas.getWidth() / 2) / xScale; x <= (polynomialCanvas.getWidth() / 2) / xScale; x += 0.01) {
             double y = polynomialToDraw.functionValue(x);
             graphicsContext.strokeLine(adaptXCoordinate(lastX), adaptYCoordinate(lastY), adaptXCoordinate(x), adaptYCoordinate(y));
+            //Sets the ending coordinates of the drawn line to be the starting point of the next line
             lastX = x;
             lastY = y;
         }
     }
 
-    private double adaptXCoordinate(double mathematicalXCoordinate){
+    /**
+     *Adapts mathematical x coordinate to coordinates of a canvas with the origin point at the center
+     */
+    private double adaptXCoordinate(double mathematicalXCoordinate) {
         return mathematicalXCoordinate * xScale + polynomialCanvas.getWidth() / 2;
     }
 
-    private double adaptYCoordinate(double mathematicalYCoordinate){
+    /**
+     *Adapts mathematical y coordinate to coordinates of a canvas with the origin point at the center
+     */
+    private double adaptYCoordinate(double mathematicalYCoordinate) {
         return -mathematicalYCoordinate * yScale + polynomialCanvas.getHeight() / 2;
     }
 }
