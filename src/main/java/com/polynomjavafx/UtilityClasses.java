fe08c1fa79444c8dceb8fa462fe76d5aa7fb731e
Christian <chrisfehr16@gmail.com>
2023-05-24 11:53:45 +0200

    Add UtilityClass

---
+++ src/main/java/com/polynomjavafx/UtilityClasses.java
@@ -0,0 +1,6 @@
+package com.polynomjavafx;
+
+public class  UtilityClasses {
+    public static double roundToSecondDecimalPoint(double number) {
+        return Math.round(number * 100.0) / 100.0;    }
+}
